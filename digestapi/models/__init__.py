e270152b0335c5d41c57ce2a5ede5199d24744f2
Zach D'Amico <zachdamico90@gmail.com>
2023-11-08 10:17:31 -0600

    login and registation

--- digestapi/models/__init__.py
+++ digestapi/models/__init__.py
@@ -2,3 +2,5 @@ from .book import Book
 from .review import Review
 from .category import Category
 from .bookCategory import BookCategory
+# once models ready for migration remember to name api when making migration command 
+# ex- this was digestapi
